8b3c570027ef49145f40151037fefcfc655b3671
Sylvain Corlay <sylvain.corlay@gmail.com>
2023-02-09 16:39:58 +0100

    Add configuration files for official list

--- recipe/build_parts.bat
+++ recipe/build_parts.bat
@@ -8,4 +8,9 @@ mkdir %LIBRARY_PREFIX%\share\ldraw\models
 xcopy p\* %LIBRARY_PREFIX%\share\ldraw\p\ /s /y
 xcopy parts\* %LIBRARY_PREFIX%\share\ldraw\parts\ /s /y
 xcopy models\* %LIBRARY_PREFIX%\share\ldraw\models\ /s /y
+
+xcopy LDConfig.ldr %LIBRARY_PREFIX%\share\ldraw\ /s /y
+xcopy LDCfgalt.ldr %LIBRARY_PREFIX%\share\ldraw\ /s /y
+xcopy LDConfig_TLG.ldr %LIBRARY_PREFIX%\share\ldraw\ /s /y
+
 if errorlevel 1 exit 1
